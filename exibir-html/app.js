531db8dcc3f004707629880ca5f460727384c00b
GabrielAlvesFR <gabrielalvesdefreitas24@gmail.com>
2025-09-07 17:20:14 -0300

    exibir-html update

--- exibir-html/app.js
+++ exibir-html/app.js
@@ -2,9 +2,12 @@ const express = require("express")
 const app = express()
 
 app.get("/", function(req, res){
-    res.sendFile(__dirname + "/exibir-html/index.html") //retorna o index, a raiz da aplicação
+    res.sendFile(__dirname + "/index.html") //retorna o index, a raiz da aplicação
 });
 
+app.get("/sobre", function(req, res){
+    res.sendFile(__dirname + "/sobre.html") //dirname é a variavel global da raiz do arquivo
+})
 app.listen(8055, function(){
     console.log("Funcionando")
 })
